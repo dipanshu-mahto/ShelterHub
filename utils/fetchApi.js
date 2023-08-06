8b1ebb7e0b05ef9e099d245eb34d00653f7f46a1
dipan <dipanshu.ug20.ece@nitp.ac.in>
2023-08-06 16:50:47 +0530

    6/8/2023

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,8 +5,8 @@ export const baseUrl = 'https://bayut.p.rapidapi.com';
 export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
-      'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': 'a6577cd029msh1cc3f0bc1c21b0cp10c3ecjsnb5776d1020c0'||process.env.NEXT_PUBLIC_RAPID_API_KEY,
+      'x-rapidapi-host': process.env.NEXT_PUBLIC_RAPID_API_HOST,
+      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY,
     },
   });
     
